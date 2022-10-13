dfeeb508a5cb12d6910477a6ac0d33eeadb8d30b
Martin Huerta <martinhuertajr@gmail.com>
2022-10-13 15:13:20 -0500

    first commit

---
+++ tests/employee.test.js
@@ -0,0 +1,27 @@
+const { default: test } = require('node:test');
+const { hasUncaughtExceptionCaptureCallback } = require('process');
+const Employee = require('../lib/employee');
+const newEmployee = new Employee ('john', '1234', 'johnsummit@gmail.com');
+
+
+test('test if we can get the constructor values for the employee object', () => {
+    expect(employee.name).toBe('john');
+    expect(employee.id).toBe('1234');
+    expect(employee.email).toBe('johnsummit@gmail.com');
+});
+
+test('test if we can get the name from the getName() method', () => {
+    expect(employee.getName()).toBe('john');
+});
+
+test('test if we can get the id from the getId() method', () => {
+    expect(employee.getId()).toBe('1234');
+});
+
+test('test if we can get the email from the getEmail() method', () => {
+    expect(employee.getEmail()).toBe('johnsummit@gmail.com');
+});
+
+test('test if we can get the rolde from the getRole() method', () => {
+    expect(employee.getRole()).toBe('Employee');
+});
